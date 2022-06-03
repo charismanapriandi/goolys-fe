9ffb9bee2677a722b51d1810f0b33f08c2184eff
Risman <rismantrdr@gmail.com>
2022-06-03 16:50:33 +0800

    fix cursor

--- components/atoms/Cursor.tsx
+++ components/atoms/Cursor.tsx
@@ -1,63 +1,55 @@
 import { useContext, useEffect, useRef, useState } from "react"
-import styled, { useTheme } from "styled-components"
+import styled, { css, keyframes, useTheme } from "styled-components"
 import gsap from 'gsap';
 import { CursorContext } from "context/cursorContext";
-// import { CursorContext } from "context/cursorContext";
-// import IconCollection from "./IconCollection";
-// import { CursorText } from "context/interface/cursor.interface";
 
-const Root = styled('div')<{isClickable: boolean}>(({theme, isClickable}) => `
-  .Cursor-dot {
-    position: fixed;
-    width: 20px;
-    height: 20px;
-    transform: translate(-50%, -50%);
-    border-radius: 50%;
-    background-color: #fff;
-    pointer-events: none;
-    z-index: 1000;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    mix-blend-mode: difference;
+const rotateCircle = keyframes({
+  'from': {
+    transform: 'rotate(0deg)',
+  },
+  'to': {
+    transform: 'rotate(360deg)',
   }
-  .Cursor-circle_root {
-    position: fixed;
-    width: 60px;
-    height: 60px;
-    transform: translate(-50%, -50%);
-    border-radius: 60px;
-    transition: top 0.1s, left 0.1s, width .4s ease-in-out, height .4s ease-in-out;
-    z-index: 1000;
-    pointer-events: none;
-    .Cursor-circle {
-      mix-blend-mode: difference;
-      fill: transparent;
-      stroke-dasharray: 1 15;
-      stroke-width: 3;
-      stroke-linecap: round;
-      transform-origin: center;
-      stroke: ${theme.colors.white.opacity[1]};
-      animation: circle-rotate 15s linear infinite;
-    }
+})
+
+const Root = styled('div')<{isClickable: boolean}>(({theme}) => ({
+  '.cursor-dot': {
+    position: 'fixed',
+    width: '20px',
+    height: '20px',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '50%',
+    backgroundColor: '#fff',
+    pointerEvents: 'none',
+    zIndex: '1000',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    mixBlendMode: 'difference',
+  },
+  '.cursor-circle_root': {
+    position: 'fixed',
+    width: '60px',
+    height: '60px',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '60px',
+    transition: 'top 0.1s, left 0.1s, width .4s ease-in-out, height .4s ease-in-out',
+    zIndex: '1000',
+    pointerEvents: 'none',
+    fill: 'transparent',
+    mixBlendMode: 'difference',
+    '.cursor-circle': {
+      fill: 'transparent',
+      strokeDasharray: '1 15',
+      strokeWidth: 3,
+      strokeLinecap: 'round',
+      transformOrigin: 'center',
+      stroke: theme.colors.white.opacity[1],
+    },
   }
-  .Cursor-text {
-    position: fixed;
-    width: 60px;
-    height: 60px;
-    color: red;
-    transform: translate(-50%, -50%);
-    transition: top 0.1s, left 0.1s, width .4s ease-in-out, height .4s ease-in-out;
-    z-index: 1000;
-    pointer-events: none;
-  }
-  @keyframes circle-rotate {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+}), css`
+  .cursor-circle {
+    animation: ${rotateCircle} 15s linear infinite
   }
 `)
 
@@ -65,7 +57,6 @@ const Cursor = () => {
   const cursorCtx = useContext(CursorContext);
   const dotRef = useRef(null)
   const circleRef = useRef(null)
-  const textRef = useRef(null)
   
   const growCursor = () => {
     gsap.to(dotRef.current, {
@@ -93,7 +84,7 @@ const Cursor = () => {
       ease: "power2",
       css: {
         transform: 'translate(-50%, -50%) scale(1.5)',
-        opacity: 0
+        opacity: 1
       }
     })
   }
@@ -110,21 +101,14 @@ const Cursor = () => {
   
   const handleMouseMove = (e: MouseEvent) => {
     gsap.to(dotRef.current, {
-      duration: 1,
+      duration: .5,
       css: {
         left: e.clientX,
         top: e.clientY
       }
     });
     gsap.to(circleRef.current, {
-      duration: 1,
-      css: {
-        left: e.clientX,
-        top: e.clientY
-      }
-    });
-    gsap.to(textRef.current, {
-      duration: 1,
+      duration: .6,
       css: {
         left: e.clientX,
         top: e.clientY
@@ -153,14 +137,11 @@ const Cursor = () => {
   }, [cursorCtx.text])
 
   return (
-    <Root className='Cursor-root' isClickable={cursorCtx.text !== null}>
-      <div ref={dotRef} className="Cursor-dot" />
-      <svg viewBox="0 0 60 60" ref={circleRef} id={'Cursor-circle'} className="Cursor-circle_root">
-        <circle id="Cursor_circle" className="Cursor-circle" r='30px' cx='50%' cy="50%" />
+    <Root className='cursor-root' isClickable={cursorCtx.text !== null}>
+      <div ref={dotRef} className="cursor-dot" />
+      <svg viewBox="0 0 60 60" ref={circleRef} className="cursor-circle_root">
+        <circle id="cursor_circle" className="cursor-circle" r='30px' cx='50%' cy="50%" />
       </svg>
-      {/* <div ref={textRef} className="Cursor-text">
-        {cursorCtx.text}
-      </div> */}
     </Root>
   )
 }
